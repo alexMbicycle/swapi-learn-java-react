4090842d4cc3682a016ee64139bddc762fd5b41b
RomanTrukhanovich <roman.trukhanovich@gmail.com>
2023-02-10 15:00:34 +0300

    Move images to another folder

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -32,7 +32,7 @@ function Header(): JSX.Element {
           <Item>
             <Avatar
               alt="photo"
-              src="./public/static/resources/avatar/photo.png"
+              src="./public/static/images/photo.png"
               sx={{
                 width: 190,
                 height: 190,
